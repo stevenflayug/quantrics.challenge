5fd80747ff752cda43cd62b731fa0bb323e63aad
Steven Layug <StevenLayug@CT-ADMINs-MacBook-Pro-2.local>
2021-04-08 00:21:04 +0800

    Added pods, initial changes for API and Networking

---
+++ yelp locator/yelp locator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yelp Locator
+//
+//  Created by Steven Layug on 7/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yelp locator/yelp locator/yelp_locatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_LocatorApp.swift
+//  Yelp Locator
+//
+//  Created by Steven Layug on 7/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_LocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
